13f636fa07980ef0f0113bcd1a2bfa5330bef706
Muhammad Ramish <mramish9696@gmail.com>
2024-02-19 16:22:59 +0500

    slowly improving in app main functioality for better handling

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -1,12 +1,13 @@
 using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Purchasing;
 using UnityEngine.UI;
 
 
 [Serializable]
-public class ConsumableItem {
+public class ConsumableItem
+{
 
     public string id;
     public string name;
@@ -41,22 +42,68 @@ public class StoreManager : MonoBehaviour, IStoreListener
     public NonConsumableItem nonConsumableItem;
     public SubscriptionItem subscriptionItem;
 
-    [Header("Inventory Items")]
-    public Text coinsText;
+    public List<string> InAppProducts;
 
     IStoreController storeController;
+    public static StoreManager _instance;
+    public static StoreManager Instance
+    {
+
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<StoreManager>();
+                if (_instance != null)
+                {
+                    DontDestroyOnLoad(_instance.gameObject);
+                }
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+
+    }
     private void Start()
     {
-        SetUI();
+        //SetUI();
         SetupBuilder();
     }
-
-    private void SetUI()
+    public void BuyProduct(int num)
     {
 
-        coinsText.text = PlayerPrefs.GetInt("Coins").ToString();
+        BuyProductID(InAppProducts[num]);
     }
-    public void ConsumableButtonPressed() {
+    private void BuyProductID(string MproductID)
+    {
+
+        if (IsInitialized())
+        {
+
+            Product product = storeController.products.WithID(MproductID);
+
+            if (product != null && product.availableToPurchase)
+            {
+
+                storeController.InitiatePurchase(product);
+                Debug.Log($"Product with product ID: {product.definition.id} is being initiated for purchase");
+            }
+            else
+            {
+
+                Debug.Log($"Product with product ID: {product.definition.id} has FAILED to initiate for purchase");
+            }
+        }
+    }
+    public void ConsumableButtonPressed()
+    {
 
         storeController.InitiatePurchase(consumableItem.id);
     }
@@ -72,41 +119,47 @@ public class StoreManager : MonoBehaviour, IStoreListener
     }
     public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs purchaseEvent)
     {
+        PurchaseProcessingResult flag = PurchaseProcessingResult.Pending;
         var product = purchaseEvent.purchasedProduct;
-        if (product.definition.id == consumableItem.id) {
-        
-
-        }
-        else if (product.definition.id == nonConsumableItem.id)
+        for (int i = 0; i < InAppProducts.Count; i++)
         {
 
+            if (product.definition.id == InAppProducts[i])
+            {
 
+                Debug.Log($"Product with product ID: {InAppProducts[i]} has been successfully purchased");
+                flag = PurchaseProcessingResult.Complete;
+                break;
+            }
         }
-        else if (product.definition.id == subscriptionItem.id)
-        {
 
-
-        }
-        return PurchaseProcessingResult.Complete;
+        return flag;
     }
 
-    private void SetupBuilder() {
+    private void SetupBuilder()
+    {
 
+        if (IsInitialized())
+        {
+            var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
 
-        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+            /*builder.AddProduct(consumableItem.id, ProductType.Consumable);
+            builder.AddProduct(nonConsumableItem.id, ProductType.NonConsumable);
+            builder.AddProduct(subscriptionItem.id, ProductType.Subscription);*/
 
-        builder.AddProduct(consumableItem.id, ProductType.Consumable);
-        builder.AddProduct(nonConsumableItem.id, ProductType.NonConsumable);
-        builder.AddProduct(subscriptionItem.id, ProductType.Subscription);
+            UnityPurchasing.Initialize(this, builder);
+        }
 
-        UnityPurchasing.Initialize(this, builder);
-    
-    
+    }
+    private bool IsInitialized()
+    {
+
+        return storeController != null;
     }
     public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
     {
         UnityEngine.Debug.Log("Store controller initialized");
-        storeController= controller;
+        storeController = controller;
     }
     public void OnInitializeFailed(InitializationFailureReason error)
     {
@@ -118,11 +171,11 @@ public class StoreManager : MonoBehaviour, IStoreListener
         UnityEngine.Debug.Log("Purchase Failed");
     }
 
-    
+
     public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
     {
         UnityEngine.Debug.Log("Purchase Failed");
     }
 
-   
+
 }
--- Assets/Scripts/StoreUIManager.cs
+++ Assets/Scripts/StoreUIManager.cs
@@ -1,8 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoreUIManager : MonoBehaviour
 {
-    
+    [Header("In App Texts")]
+    public Text coinText;
+    public Text coin500Price;
+    public Text newBikePrice;
+    public Text elitePrice;
 }
